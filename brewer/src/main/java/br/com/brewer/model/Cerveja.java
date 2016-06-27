671185085500b5d0b217c4c398463bd38aedd358
Silvan de Jesus <silvanmtr@hotmail.com>
2016-06-27 14:30:55 -0300

    Prototipando tela de cadastro de cerveja.

---
+++ brewer/src/main/java/br/com/brewer/model/Cerveja.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package br.com.brewer.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ * @author Silvan de Jesus
+ *
+ */
+public class Cerveja {
+
+	@NotBlank(message = "SKU é obrigatório")
+	private String sku;
+	
+	@NotBlank(message = "Nome é obrigatório")
+	private String nome;
+	
+	@Size(min = 1, max = 50, message = "O tamanho da descrição deve estar entre 1 e 50")
+	private String descricao;
+
+	public Cerveja() {
+
+	}
+
+	public String getSku() {
+		return sku;
+	}
+
+	public void setSku(String sku) {
+		this.sku = sku;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
